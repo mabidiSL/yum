ed713e5edaecb1f432fa6a892f215299c9455ebe
= <=>
2024-08-22 11:55:44 +0100

    forgot pwd from pin to link 3

--- routes/crud.js
+++ routes/crud.js
@@ -417,7 +417,7 @@ exRoute.post('/forgot-password', async (req, res) => {
         await user.save();
 
         // Create the reset link
-        const resetLink = `https://rad-slrad.koyeb.app/reset-password/${resetToken}`;
+        const resetLink = `http://localhost:4200/auth/reset-password/${resetToken}`;
 
         // Send the email
         const mailOptions = {
